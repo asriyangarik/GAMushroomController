f7f2529a88447219db90ca25ae93ea05f46b9c33
Garik <asriyangarik@rambler.ru>
2023-09-07 14:43:33 +0400

    newBrench

---
+++ MyМushroomController/ParentFormGLOBAL.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace MyМushroomController
+{
+    public partial class ParentFormGLOBAL : Form
+    {
+        
+
+
+        public ParentFormGLOBAL()
+        {
+            InitializeComponent();
+        }
+
+
+
+    }
+}
--- MyМushroomController/Sections.Designer.cs
+++ MyМushroomController/Sections.Designer.cs
@@ -28,74 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Sections4RB = new System.Windows.Forms.RadioButton();
+            this.Sections3RB = new System.Windows.Forms.RadioButton();
+            this.Sections2RB = new System.Windows.Forms.RadioButton();
+            this.Sections1RB = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // groupBox1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(58, 48);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(95, 20);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.groupBox1.BackColor = System.Drawing.Color.PowderBlue;
+            this.groupBox1.Controls.Add(this.Sections4RB);
+            this.groupBox1.Controls.Add(this.Sections3RB);
+            this.groupBox1.Controls.Add(this.Sections2RB);
+            this.groupBox1.Controls.Add(this.Sections1RB);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 194);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
             // 
-            // checkBox2
+            // Sections4RB
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(58, 75);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(95, 20);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.Sections4RB.AutoSize = true;
+            this.Sections4RB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sections4RB.Location = new System.Drawing.Point(30, 152);
+            this.Sections4RB.Name = "Sections4RB";
+            this.Sections4RB.Size = new System.Drawing.Size(115, 29);
+            this.Sections4RB.TabIndex = 3;
+            this.Sections4RB.TabStop = true;
+            this.Sections4RB.Text = "4 Section";
+            this.Sections4RB.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // Sections3RB
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(58, 111);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(95, 20);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "checkBox3";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.Sections3RB.AutoSize = true;
+            this.Sections3RB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sections3RB.Location = new System.Drawing.Point(30, 108);
+            this.Sections3RB.Name = "Sections3RB";
+            this.Sections3RB.Size = new System.Drawing.Size(115, 29);
+            this.Sections3RB.TabIndex = 2;
+            this.Sections3RB.TabStop = true;
+            this.Sections3RB.Text = "3 Section";
+            this.Sections3RB.UseVisualStyleBackColor = true;
             // 
-            // checkBox4
+            // Sections2RB
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(58, 138);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(95, 20);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "checkBox4";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.Sections2RB.AutoSize = true;
+            this.Sections2RB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sections2RB.Location = new System.Drawing.Point(30, 64);
+            this.Sections2RB.Name = "Sections2RB";
+            this.Sections2RB.Size = new System.Drawing.Size(115, 29);
+            this.Sections2RB.TabIndex = 1;
+            this.Sections2RB.TabStop = true;
+            this.Sections2RB.Text = "2 Section";
+            this.Sections2RB.UseVisualStyleBackColor = true;
+            // 
+            // Sections1RB
+            // 
+            this.Sections1RB.AutoSize = true;
+            this.Sections1RB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sections1RB.Location = new System.Drawing.Point(30, 20);
+            this.Sections1RB.Name = "Sections1RB";
+            this.Sections1RB.Size = new System.Drawing.Size(115, 29);
+            this.Sections1RB.TabIndex = 0;
+            this.Sections1RB.TabStop = true;
+            this.Sections1RB.Text = "1 Section";
+            this.Sections1RB.UseVisualStyleBackColor = true;
             // 
             // Sections
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(246, 351);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(196, 218);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Sections";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Sections";
+            this.Text = "Set Sections";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Sections_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Sections3RB;
+        private System.Windows.Forms.RadioButton Sections2RB;
+        private System.Windows.Forms.RadioButton Sections1RB;
+        private System.Windows.Forms.RadioButton Sections4RB;
     }
 }
